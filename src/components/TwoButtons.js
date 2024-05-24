c8ac2f3d3be2056fc15cc8c8449a5bcff9fa6771
astacus777 <astacus@hotmail.co.uk>
2024-05-24 10:06:39 +0200

    TwoButtons - links added

--- src/components/TwoButtons.js
+++ src/components/TwoButtons.js
@@ -1,10 +1,16 @@
 import React from "react";
 import '../scss/NavBar.scss';
+import { Link as RouterLink } from "react-router-dom";
 const TwoButtons = () => {
     return (
         <div className="big-button-container">
+            <RouterLink to="/logowanie">
             <button className="big-button">Oddaj rzeczy</button>
+            </RouterLink>
+
+                <RouterLink to="/logowanie">
             <button className="big-button">Zorganizuj zbiórkę</button>
+                </RouterLink>
         </div>
     )
 }
