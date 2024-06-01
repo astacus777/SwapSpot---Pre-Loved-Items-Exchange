fb1272f920fbadea39f43512110dacbea51052ce
astacus777 <astacus@hotmail.co.uk>
2024-06-01 23:05:24 +0200

    FundOrgCol component added

---
+++ src/data/fundOrgColData.js
@@ -0,0 +1,28 @@
+// src/data/fundOrgColData.js
+
+export const foundationsData = [
+    { id: 1, name: "Dbam o Zdrowie", text: "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.", info: "ubrania, jedzenie, sprzęt AGD, meble, zabawki" },
+    { id: 2, name: "Dla dzieci", text: "Cel i misja: Pomoc dzieciom z ubogich rodzin.", info: "ubrania, meble, zabawki"  },
+    { id: 3, name: "Bez domu", text: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.", info: "ubrania, jedzenie, ciepłe koce" },
+    { id: 4, name: "Lorem Ipsum 11", text: "Fundacje - Wpis 4", info: "Mi, quis, hendrerit, dolor"  },
+    { id: 5, name: "Lorem Ipsum 1326", text: "Fundacje - id diam vel quam elementum pulvinar 5", info: "Ut, aliquam, purus, sit, amet"  },
+    { id: 6, name: "Lorem Ipsum 1 45", text: "Fundacje - id diam vel quam elementum pulvinar 6", info: "Ut, aliquam, purus, sit, amet"  },
+    { id: 7, name: "Lorem Ipsum 1 69", text: "Fundacje - id diam vel quam elementum pulvinar 7", info: "Ut, aliquam, purus, sit, amet" },
+    { id: 8, name: "Lorem Ipsum 777", text: "Fundacje - id diam vel quam elementum pulvinar 8" , info: "Ut, aliquam, purus, sit, amet" },
+    { id: 9, name: "Lorem Ipsum 1987", text: "Fundacje - id diam vel quam elementum pulvinar 9" , info: "Ut, aliquam, purus, sit, amet" }
+];
+
+export const organisationsData = [
+    { id: 1, name: "Organizacja : Lorem Ipsum 1", text: "Quis varius quam quisque id diam vel quam elementum pulvinar." , info: "Egestas, sed, tempus"},
+    { id: 2, name: "Lorem Ipsum 2", text: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.", info: "Ut, aliquam, purus, sit, amet" },
+    { id: 3, name: "Lorem Ipsum 3", text: "Scelerisque in dictum non consectetur a erat nam.", info: "Mi, quis, hendrerit, dolor"  },
+    { id: 4, name: "Lorem Ipsum 4", text: "Quis varius quam quisque id diam vel quam elementum pulvinar.", info: "Ut, aliquam, purus, sit, amet" },
+    { id: 5, name: "Lorem Ipsum 5", text: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue." , info: "Ut, aliquam, purus, sit, amet" },
+    { id: 6, name: "Lorem Ipsum 6", text: "Quis varius quam quisque id diam vel quam elementum pulvinar.", info: "Ut, aliquam, purus, sit, amet" }
+];
+
+export const collectionsData = [
+    { id: 1, name: "Zbiórka: Lorem Ipsum 1", text: "Quis varius quam quisque id diam vel quam elementum pulvinar.", info: "Egestas, sed, tempus" },
+    { id: 2, name: "Zbiórka: Lorem Ipsum 2", text: "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.", info: "Ut, aliquam, purus, sit, amet"  },
+    { id: 3, name: "Zbiórka : Lorem Ipsum 3", text: "Scelerisque in dictum non consectetur a erat nam.", info: "Mi, quis, hendrerit, dolor"  },
+];
